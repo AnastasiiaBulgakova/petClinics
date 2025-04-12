af018bf2753d19e65ebe692674a5d920678453f3
Anastasiia Bulgakova <nbulghakova@mail.ru>
2025-04-12 14:10:34 +0700

    pusfhdfs

--- src/shared/services/api/lib/consts.ts
+++ src/shared/services/api/lib/consts.ts
@@ -1,8 +1,7 @@
-export const baseUrl = 
-  process.env.NODE_ENV === 'development' 
-    ? 'http://localhost:3001/api'   // Локальный сервер для разработки
-    : '/api';                       // Для Vercel и продакшн-серверов
-
+export const baseUrl =
+  import.meta.env.DEV
+    ? 'http://localhost:3001/api'
+    : import.meta.env.VITE_API_URL;
 export const pets = ['Кошки', 'Собаки', 'Птицы', 'Грызуны', 'Рептилии', 'Кролики', 'Хорьки'];
 export const petsBlockCallDoc = 'Вызвать доктора на дом';
 export const petsBlockWorkwAllTypes = 'Работаем со всеми видами животных';
