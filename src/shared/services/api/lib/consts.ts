ae93405acc5ef6e5c248741a9ed0bfa9a08ec51b
Anastasiia Bulgakova <nbulghakova@mail.ru>
2025-04-12 14:16:05 +0700

    pusfhdfsfd

--- src/shared/services/api/lib/consts.ts
+++ src/shared/services/api/lib/consts.ts
@@ -1,7 +1,8 @@
 export const baseUrl =
   import.meta.env.DEV
     ? 'http://localhost:3001/api'
-    : import.meta.env.VITE_API_URL;
+    : '/api'; // ⚠️ Только так!
+
 export const pets = ['Кошки', 'Собаки', 'Птицы', 'Грызуны', 'Рептилии', 'Кролики', 'Хорьки'];
 export const petsBlockCallDoc = 'Вызвать доктора на дом';
 export const petsBlockWorkwAllTypes = 'Работаем со всеми видами животных';
