a022fae7a5e9f8832080e31482b970e4120ab9b4
Anastasiia Bulgakova <nbulghakova@mail.ru>
2025-04-12 13:34:15 +0700

    d

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,8 +5,11 @@ import Header from '../widgets/Header/Header.tsx';
 
 import { ThemeSwitcher, ThemeProvider } from '@/features';
 import Footer from '@/widgets/Footer/Footer.tsx';
+import { baseUrl } from '@/shared/services/api/lib/consts.ts';
 
 const App: FC = () => {
+  console.log('BASE URL:', baseUrl);
+
   return (
     <ThemeProvider>
       <Header />
