e875d7d3fe3486bd9eafda6d65d27443ac24bdac
Anastasiia Bulgakova <nbulghakova@mail.ru>
2025-04-12 13:28:21 +0700

    fixing paths

--- src/shared/services/api/lib/consts.ts
+++ src/shared/services/api/lib/consts.ts
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://91.241.64.154:8080/api/';
+export const baseUrl = import.meta.env.VITE_API_URL || 'http://localhost:3001/api/';
 export const pets = ['Кошки', 'Собаки', 'Птицы', 'Грызуны', 'Рептилии', 'Кролики', 'Хорьки'];
 export const petsBlockCallDoc = 'Вызвать доктора на дом';
 export const petsBlockWorkwAllTypes = 'Работаем со всеми видами животных';
